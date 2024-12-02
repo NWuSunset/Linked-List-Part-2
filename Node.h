0c4f9aefb9d415e6dda18d92429a11aa9b9314ad
NWuSunet <476275@bsd48.org>
2024-12-02 11:11:49 -0800

    first commit

---
+++ Node.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Student.h"
+
+
+class Node {
+ public:
+  Node(Student*); //Constructor that takes a Student pointer. Sets 'student' to the passed in pointer
+  ~Node(); //Destructor. Deletes 'next' and 'student'
+
+  Node(); //default constructor 
+  Node* getNext(); //get next Node pointer. returns 'next'
+  Student* getStudent(); //get student pointer. returns 'student'
+  void setNext(Node*); //set the next pointer to the corresponding node pointer. 'next' is set to the passed in pointer
+
+ private:
+  Student* student; // 'student'
+  Node* next; // 'next'
+};
+
