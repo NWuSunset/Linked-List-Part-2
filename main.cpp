1f56c54a45288ccf21a172cb5eb727b3d69792f7
BuildTools <unconfigured@null.spigotmc.org>
2024-12-04 21:28:08 -0800

    WOrking on adding. Currently getting a seg fault when adding in the addNOdes function (addign more than one node)

--- main.cpp
+++ main.cpp
@@ -5,31 +5,41 @@
 
 using namespace std;
 
-void Add(Node* &head);
-void Print();
+void Add(Node* &head, Node* curr);
+void Print(Node* node);
 void Delete();
-void addNodes(Node* &node)
+void addNodes(Node* &head, Node* curr, Node* node);
 
 int main() {
-  Node* head = new Node; //create the first (header) node in the list
-  
-  char input[7]; 
-  
-  cout << "Do you want to ADD, PRINT, or DELETE?" << endl;
-  cin.getline(input, 7);
+  Node* head = nullptr; //create the first (header) node in the list
+  Node* current = head; //Current node starts as the head node
+  bool quit = false;
 
-  if (strcasecmp(input, "ADD") == 0) {
-    Add(head);
-  } else if (strcasecmp(input, "PRINT") == 0) {
-    Print();
-  } else if (strcasecmp(input, "DELETE") == 0) {
-    Delete();
+  char input[7]; 
+
+  while (quit == false) {
+    cout << "Do you want to ADD, PRINT, DELETE, or QUIT" << endl;
+    cin.getline(input, 7);
+
+    if (strcasecmp(input, "ADD") == 0) {
+      Add(head, current);
+    } else if (strcasecmp(input, "PRINT") == 0) {
+      if (head != nullptr) {
+        Print(head);
+      } else {
+        cout << "There are no students in the list" << endl;
+      }
+    } else if (strcasecmp(input, "DELETE") == 0) {
+      Delete();
+    } else if (strcasecmp(input, "QUIT") == 0) {
+      quit = true;
+    }
   }
   
   return 0;
 }
 
-void Add(Node* &head) {
+void Add(Node* &head, Node* curr) {
   //individual variables for clarity
   char firstName[20];
   char lastName[20];
@@ -47,52 +57,61 @@ void Add(Node* &head) {
   cin.ignore(); 
 
   Student* student = new Student(firstName, lastName, id, gpa); //Create a new student with the information
-    
-  //Sort by id number?
-  sortNodes(head, student);
+  Node* node = new Node(student); //create a new node based on the student
+
+  addNodes(head, curr, node); //Make sure the nodes will be sorted into the list correctly
 }
 
+//Go though all the nodes in the list using recursion
+void Print(Node* node) {
+  //test printing first:
+  cout << node->getStudent()->getId() << endl;
 
-void Print() {
 
-
+  if (node->getNext() != nullptr) {
+  Print(node->getNext());
+    }
 }
 
 void Delete() {
   
 }
 
-//First node to be passed in will be the head node
-void addNodes(Node* &node, Student* student) { //Used to sort the nodes
-  if (node == nullptr) { //if node is null add the new node to it
-    node = new Node(student);
-    return;
-  } 
+//Sorts the list from lowest to highest student id.
+void addNodes(Node* &head, Node* curr, Node* node) { //Used to sort the nodes
+  if (head == nullptr) { //if head node is null add the new node to it
+    head = node; //The node passed in will now be the head node
+    curr = node; //update the current node (to also not be null)
+    Print(head);
+    return; //Exit the function
+  }
+  Print(head);
 
-  if (node->getNext() == nullptr) { //check if next node is null
-    node->setNext(new Node(student));
+  if (curr->getNext() == nullptr) {
+    curr->setNext(node); //if next node in the list is null; set it to the passed in node
+  }
+  Print(head);
 
-    
-    //If the student that would be getting passed into the node has a id less than the next student in the nodeList
-    if (student->getId() < node->getStudent()->getId()) {
-      //Swap the node order
-      node->setNext(node); //set the next node to this node
-      node = new Node(student); //now set this node to the node that would be created
-    }
-  } 
+  //If the passed in node is less than the head node.
+  if (node->getStudent()->getId() < head->getStudent()->getId()) {
+  //Swap their positions
+    Node* temp = head;
+    head = node;
+    node = temp;
+  }
+  Print(head);
 
 
+  //If the next node is greater than the node passed in we want to swap their places in the list
+  if (curr->getNext()->getStudent()->getId() > node->getStudent()->getId()) {
+    node->setNext(curr->getNext()); //Then set the greater node (next node) after the passed in node
+    curr->setNext(node); //Then the node after current will be set to the passed in node
+    curr = curr->getNext(); //increment the current node up one
+    addNodes(head, curr, node);
+  }
+  Print(head);
 
 
-  /*
-  Node* temp = node->getNext(); //save the next node pointer
-
-  //if this id is greater (we will move it up in the list by swapping the two nodes)
-  if (node->getStudent()->getId() > node->getNext()->getStudent()->getId() ) {
-    node->setNext(node); //set the next node to this node
-    node = temp; //set this node to the original nextNode
-
-    //Call the function again to sort the next nodes in the list
-    sortNodes(node->getNext());
-    } */
+  //A while loop going through the list is technically also valid
+  //While curr < node
 }
